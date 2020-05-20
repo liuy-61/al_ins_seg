93ef6ac72d1ec5a74872eab51af06030bfb42bce
tangypnuaa <tangyp@nuaa.edu.cn>
2020-05-20 22:15:00 +0800

    copy losssampler from coreset

--- liuy/implementation/LossSampler.py
+++ liuy/implementation/LossSampler.py
@@ -1,5 +1,6 @@
 from liuy.Interface.BaseSampler import BaseSampler
 from liuy.utils.ComputeLoss import LiuyComputeLoss
+
 import re
 
 
@@ -17,23 +18,35 @@ def name2id(losses_name):
     return losses_id
 
 
-def sort_losses(losses):
+def decrease_sort_losses(losses):
     if len(losses) <= 1:
         return losses
     pivot = losses[len(losses) // 2]
     left = [x for x in losses if x['loss_mask'] > pivot['loss_mask']]
     middle = [x for x in losses if x['loss_mask'] == pivot['loss_mask']]
     right = [x for x in losses if x['loss_mask'] < pivot['loss_mask']]
-    return sort_losses(left) + middle + sort_losses(right)
+    return decrease_sort_losses(left) + middle + decrease_sort_losses(right)
+
+def increase_sort_losses(losses):
+    if len(losses) <= 1:
+        return losses
+    pivot = losses[len(losses) // 2]
+    left = [x for x in losses if x['loss_mask'] < pivot['loss_mask']]
+    middle = [x for x in losses if x['loss_mask'] == pivot['loss_mask']]
+    right = [x for x in losses if x['loss_mask'] > pivot['loss_mask']]
+    return increase_sort_losses(left) + middle + increase_sort_losses(right)
 
 
-class LossSampler(BaseSampler):
-    def __init__(self, sampler_name, data_loader,):
-        super(LossSampler, self).__init__(sampler_name, data_loader)
+class LossSampler():
+    def __init__(self, sampler_name):
+        self.sampler_name = sampler_name
 
-    def select_batch(self, n_sample, already_selected, losses):
-        losses = sort_losses(losses)
-        losses = name2id(losses)
+    def select_batch(self, n_sample, already_selected, losses, loss_decrease=True):
+        if loss_decrease:
+            losses = decrease_sort_losses(losses)
+        else:
+            losses = increase_sort_losses(losses)
+
         cnt = 0
         i = 0
         samples = []
@@ -41,8 +54,17 @@ class LossSampler(BaseSampler):
             if losses[i]['image_id'] not in already_selected and losses[i]['image_id'] not in samples:
                 samples.append(losses[i]['image_id'])
                 cnt += 1
+                if cnt%10 == 0:
+                    print("has got {} image id, still need {} image id".format(cnt, n_sample-cnt))
             i += 1
+        # while cnt < n_sample:
+        #     for loss in losses:
+        #         if loss['image_id'] not in already_selected and loss['image_id'] not in samples:
+        #             samples.append(loss['image_id'])
+        #             cnt += 1
         assert len(samples) == n_sample
         assert len(set(samples)) == len(samples)
         return samples
 
+if __name__ == '__main__':
+    pass
