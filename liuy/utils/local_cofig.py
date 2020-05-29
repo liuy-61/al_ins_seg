ca6724d4738ab718b58bb3b6f171cb29b83f6f2b
tangypnuaa <tangyp@nuaa.edu.cn>
2020-05-29 20:28:04 +0800

    新增了 MASK_RCNN2 的配置文件 为使用两个不同的模型做准备

--- liuy/utils/local_cofig.py
+++ liuy/utils/local_cofig.py
@@ -24,5 +24,8 @@ OUTPUT_DIR = '/media/tangyp/Data/model_file/OUTPUT_DIR'
 MODEL_NAME = {
     'Faster_RCNN': '/home/tangyp/liuy/detectron2_origin/configs/COCO-Detection/faster_rcnn_R_50_C4_1x.yaml',
 
-    'Mask_RCNN': '/home/tangyp/liuy/detectron2_origin/configs/COCO-InstanceSegmentation/mask_rcnn_R_50_C4_3x.yaml'
+    'Mask_RCNN': '/home/tangyp/liuy/detectron2_origin/configs/COCO-InstanceSegmentation/mask_rcnn_R_50_C4_3x.yaml',
+
+    'Mask_RCNN2': '/home/tangyp/liuy/al_ins_seg/configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml',
+
     }
